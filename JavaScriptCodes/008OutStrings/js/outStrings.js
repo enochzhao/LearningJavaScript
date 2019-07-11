dcd2e76d37701934b86bc1cd41782fb87fbd314a
enochzhao <zypsalms@sina.com>
2019-07-11 16:06:32 +0800

    声明方式测试

--- JavaScriptCodes/008OutStrings/js/outStrings.js
+++ JavaScriptCodes/008OutStrings/js/outStrings.js
@@ -1,5 +1,9 @@
-var myName1 = "enoch";//双引号
-var myName2 = 'zhao yong';//单引号
+/*多行声明*/
+//var myName1 = "enoch";//双引号
+//var myName2 = 'zhao yong';//单引号
+
+/*一行多个声明*/
+var myName1 = "enoch", myName2 = "zhao yong";
 
 document.getElementById('ex1').innerHTML = myName1;
 document.getElementById("ex2").innerHTML = myName2;

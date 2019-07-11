7a3d9d27364cb1314e62714b5a34951769a83316
enochzhao <zypsalms@sina.com>
2019-07-11 16:19:15 +0800

    number数据类型测试

---
+++ JavaScriptCodes/009NumberType/js/number.js
@@ -0,0 +1,7 @@
+var x1 = 5;			//不含小数点
+var x2 = 16.5;		//含小数点
+var x3 = 011;		//八进制
+var x4 = 0x11;		//十六进制
+
+document.getElementById("ex").innerHTML = x1 + "<br>" + x2 +
+"<br>" + x3 + "<br>" + x4;

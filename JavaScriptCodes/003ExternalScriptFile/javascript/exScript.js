b7ccb4a1f8e9949b97c6d748dec2cc784ef8bac9
enochzhao <zypsalms@sina.com>
2019-07-11 13:19:24 +0800

    新增以外部文件形式导入js测试代码
    以js为外部文件导入到HTML中

---
+++ JavaScriptCodes/003ExternalScriptFile/javascript/exScript.js
@@ -0,0 +1,7 @@
+function exFun(){
+	if (document.getElementById("ex").innerHTML == "javascript"){
+		document.getElementById("ex").innerHTML = "HTML5+CSS3";
+	}else{
+		document.getElementById("ex").innerHTML = "javascript";
+	}
+}

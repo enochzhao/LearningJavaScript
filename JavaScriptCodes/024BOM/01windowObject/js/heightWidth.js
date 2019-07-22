5994ff8b7d5aff63939871de3fd8b4cdf7c4920a
enochzhao <zypsalms@sina.com>
2019-07-22 17:38:46 +0800

    获取窗口高度和宽度

---
+++ JavaScriptCodes/024BOM/01windowObject/js/heightWidth.js
@@ -0,0 +1,7 @@
+var winHeight = window.innerHeight;
+var winWidth = window.innerWidth;
+var outHeight = window.outerHeight;
+var outWidth = window.outerWidth;
+
+document.getElementById("ex1").innerHTML = "浏览区height = " + winHeight + "浏览区width = " + winWidth;
+document.getElementById("ex2").innerHTML = "总窗口height = " + outHeight + "总窗口width = " + outWidth;
